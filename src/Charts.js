a5530ff6cedb71ffdb518766d6a7bd100f6bdaca
Matyáš Bach <matyas.bach@plus4u.net>
2019-10-12 15:37:28 +0200

    Order data for accumulated features chart

--- src/Charts.js
+++ src/Charts.js
@@ -6,17 +6,19 @@ export function getAccumulatedFeatures(startDateTime, endDateTime, features = []
 
   let point = { t: startDateTime, y: 0 };
   
-  const chartData = features.reduce((chartData, feature) => {
-    const { timestamp } = feature.properties;
-    if (point && point.t == timestamp) {
-      point.y++;
-    }
-    else {
-      point = { t: timestamp, y: point && point.y + 1 || 1 };
-      chartData.push(point);
-    }
-    return chartData;
-  }, [point]);
+  const chartData = features
+    .sort((featureA, featureB) => new Date(featureA.properties.timestamp) - new Date(featureB.properties.timestamp))
+    .reduce((chartData, feature) => {
+      const { timestamp } = feature.properties;
+      if (point && point.t == timestamp) {
+        point.y++;
+      }
+      else {
+        point = { t: timestamp, y: point && point.y + 1 || 1 };
+        chartData.push(point);
+      }
+      return chartData;
+    }, [point]);
 
   chartData.push({ t: endDateTime, y: point.y });
   chartData.forEach(item => item.t = moment(item.t).toISOString());
